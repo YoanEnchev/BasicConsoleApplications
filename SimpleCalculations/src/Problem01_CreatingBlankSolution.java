ed3462522506a3ac51ba394e2d9e6c0fc360aeff
YoanEnchev <joanbojchev@gmail.com>
2017-03-11 18:14:23 +0200

    Update Problem01_CreatingBlankSolution.java

--- SimpleCalculations/src/Problem01_CreatingBlankSolution.java
+++ SimpleCalculations/src/Problem01_CreatingBlankSolution.java
@@ -1,13 +1,4 @@
 import java.util.Scanner;
 
-public class Problem01_CreatingBlankSolution {
-    public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
-        String a = console.next();
-
-        if(a.equals("1"))
-        {
-            System.out.println("WOW");
-        }
-    }
+public class Problem01_CreatingBlankSolution {    
 }
